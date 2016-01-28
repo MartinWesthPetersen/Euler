fefb70466eb8f33120817011a1dde78493a4dee8
MartinWesthPetersen <mqt967@alumni.ku.dk>
2016-01-28 13:29:42 +0100

    Solved no. 1

---
+++ 1/Main.java
@@ -0,0 +1,24 @@
+import java.math.*;
+import java.util.*;
+
+class Solver {
+  public int solve() {
+    int sum = 0;
+		for (int i = 0; i < 1000; i += 3) {
+			sum += i;
+		}
+		for (int i = 0; i < 1000; i += 5) {
+			if (i % 15 != 0) {
+				sum += i;
+			}
+		}
+		return sum;
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    Solver s = new Solver();
+    System.out.println(s.solve());
+  }
+}
