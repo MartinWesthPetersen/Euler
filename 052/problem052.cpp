a8fbf0c77501cc384987ca92980a4b85ca882fb1
MartinWesthPetersen <mqt967@alumni.ku.dk>
2016-10-11 13:02:39 +0200

    Problem 052

--- 052/problem052.cpp
+++ 052/problem052.cpp
@@ -3,17 +3,8 @@
 #include <algorithm>
 #include <string>
 #include <sstream>
-#include <boost/algorithm/string.hpp>
 
 using namespace std;
-
-void pprint(vector<char> v) {
-  cout << "[";
-  for (vector<char>::iterator it = v.begin(); it != v.end(); it++) {
-    cout << *it << ", ";
-  }
-  cout << "]\n";
-}
 
 bool is_perm(vector<char> x, vector<char> y) {
   if (x.size() != y.size()) {
@@ -56,23 +47,6 @@ int find_number() {
 }
 
 int main() {
-  vector<char> l1;
-  l1.push_back('1');
-  l1.push_back('2');
-  l1.push_back('5');
-  l1.push_back('8');
-  l1.push_back('7');
-  l1.push_back('4');
-
-  vector<char> l2;
-  l2.push_back('2');
-  l2.push_back('5');
-  l2.push_back('1');
-  l2.push_back('7');
-  l2.push_back('4');
-  l2.push_back('8');
-
   cout << find_number() << "\n";
-
   return 0;
 }
